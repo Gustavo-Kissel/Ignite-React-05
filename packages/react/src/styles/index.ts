b030b5dd9b7a3c3043072f2d23b9f35d1f923758
Gustavo Kissel <kisselgustavo@gmail.com>
2023-09-29 15:09:10 -0300

    package-lock recreation

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@gustavokissel-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
